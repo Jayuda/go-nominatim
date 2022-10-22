6bc61bb7315dcd9e9ea0cae122d3be03351d398f
jayuda <yulius.jayuda@gmail.com>
2022-10-22 09:51:24 +0700

    first commit

---
+++ nominatim.go
@@ -0,0 +1,73 @@
+package gonominatim
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type ReverseData struct {
+	PlaceID     string   `json:"place_id"`
+	Licence     string   `json:"licence"`
+	OsmType     string   `json:"osm_type"`
+	OsmID       string   `json:"osm_id"`
+	Lat         string   `json:"lat"`
+	Lon         string   `json:"lon"`
+	PlaceRank   string   `json:"place_rank"`
+	Category    string   `json:"category"`
+	Type        string   `json:"type"`
+	Importance  string   `json:"importance"`
+	Addresstype string   `json:"addresstype"`
+	Name        string   `json:"name"`
+	DisplayName string   `json:"display_name"`
+	Address     Addresss `json:"address"`
+	BoundingBox []string `json:"boundingbox"`
+}
+
+type Addresss struct {
+	Road         string `json:"road"`
+	CityBlock    string `json:"city_block"`
+	Village      string `json:"village"`
+	Suburb       string `json:"suburb"`
+	CityDistrict string `json:"city_district"`
+	City         string `json:"city"`
+	Postcode     string `json:"postcode"`
+	Country      string `json:"country"`
+	CountryCode  string `json:"country_code"`
+}
+
+func GPSToAddress(sURL, sLat, sLong string) (ReverseData, error) {
+
+	url := sURL + "/reverse?format=jsonv2&lat=" + sLat + "&lon=" + sLong + "&zoom=16"
+	method := "GET"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+
+	if err != nil {
+		fmt.Println(err)
+		return ReverseData{}, err
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return ReverseData{}, err
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return ReverseData{}, err
+	}
+
+	// convert body to struct ReverseData
+	var data ReverseData
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		fmt.Println(err)
+		return ReverseData{}, err
+	}
+	return data, nil
+}
