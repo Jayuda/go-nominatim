2228e377d72f0f12977205fe06dd5c2d5f3e06b0
jayuda <yulius.jayuda@gmail.com>
2024-04-16 14:40:52 +0700

    clean string

--- nominatim.go
+++ nominatim.go
@@ -3,21 +3,22 @@ package gonominatim
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
+	"strings"
 )
 
 type ReverseData struct {
-	PlaceID     int   `json:"place_id"`
+	PlaceID     int      `json:"place_id"`
 	Licence     string   `json:"licence"`
 	OsmType     string   `json:"osm_type"`
-	OsmID       int   `json:"osm_id"`
+	OsmID       int      `json:"osm_id"`
 	Lat         string   `json:"lat"`
 	Lon         string   `json:"lon"`
-	PlaceRank   int   `json:"place_rank"`
+	PlaceRank   int      `json:"place_rank"`
 	Category    string   `json:"category"`
 	Type        string   `json:"type"`
-	Importance  float32   `json:"importance"`
+	Importance  float32  `json:"importance"`
 	Addresstype string   `json:"addresstype"`
 	Name        string   `json:"name"`
 	DisplayName string   `json:"display_name"`
@@ -35,6 +36,15 @@ type Addresss struct {
 	Postcode     string `json:"postcode"`
 	Country      string `json:"country"`
 	CountryCode  string `json:"country_code"`
+}
+
+// CleanString replaces single quotes with backticks in a string.
+func CleanString(s string) string {
+	s = strings.ReplaceAll(s, "'", "`")
+	s = strings.ReplaceAll(s, "\n", "")
+	s = strings.ReplaceAll(s, "\r", "")
+	s = strings.ReplaceAll(s, "\t", "")
+	return s
 }
 
 func GPSToAddress(sURL, sLat, sLong string) (ReverseData, error) {
@@ -56,7 +66,8 @@ func GPSToAddress(sURL, sLat, sLong string) (ReverseData, error) {
 	}
 	defer res.Body.Close()
 
-	body, err := ioutil.ReadAll(res.Body)
+	body, err := io.ReadAll(res.Body)
+
 	if err != nil {
 		fmt.Println(err)
 		return ReverseData{}, err
@@ -69,5 +80,19 @@ func GPSToAddress(sURL, sLat, sLong string) (ReverseData, error) {
 		fmt.Println(err)
 		return ReverseData{}, err
 	}
+
+	// check every field in data, replace "'" with "`"
+	data.Name = CleanString(data.Name)
+	data.DisplayName = CleanString(data.DisplayName)
+	data.Address.Road = CleanString(data.Address.Road)
+	data.Address.CityBlock = CleanString(data.Address.CityBlock)
+	data.Address.Village = CleanString(data.Address.Village)
+	data.Address.Suburb = CleanString(data.Address.Suburb)
+	data.Address.CityDistrict = CleanString(data.Address.CityDistrict)
+	data.Address.City = CleanString(data.Address.City)
+	data.Address.Postcode = CleanString(data.Address.Postcode)
+	data.Address.Country = CleanString(data.Address.Country)
+	data.Address.CountryCode = CleanString(data.Address.CountryCode)
+
 	return data, nil
 }
